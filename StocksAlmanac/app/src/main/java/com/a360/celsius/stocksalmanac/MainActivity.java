9af9b87b89237a526589f6b10ad953a3ed0cf4fa
dennisshar <dennis.shar@gmail.com>
2017-10-17 15:45:15 +0300

    Added afragment to each category

--- StocksAlmanac/app/src/main/java/com/a360/celsius/stocksalmanac/MainActivity.java
+++ StocksAlmanac/app/src/main/java/com/a360/celsius/stocksalmanac/MainActivity.java
@@ -1,22 +1,20 @@
 package com.a360.celsius.stocksalmanac;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-import com.a360.celsius.stocksalmanac.service.StockDataPullService;
-import com.a360.celsius.stocksalmanac.service.StockDataPullServiceIntentKeys;
-
 public class MainActivity extends BaseActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent msgIntentGetExternalIP = new Intent(getApplicationContext(), StockDataPullService.class);
-        msgIntentGetExternalIP.putExtra(StockDataPullServiceIntentKeys.DATA_TYPE_KEY,StockDataPullServiceIntentKeys.DATA_TYPE_MATERIAL_KEY);
-        startService(msgIntentGetExternalIP);
+//        Intent msgIntentGetExternalIP = new Intent(getApplicationContext(), StockDataPullService.class);
+//        msgIntentGetExternalIP.putExtra(StockDataPullServiceIntentKeys.DATA_TYPE_KEY,StockDataPullServiceIntentKeys.DATA_TYPE_MATERIAL_KEY);
+//        startService(msgIntentGetExternalIP);
 
 
     }
