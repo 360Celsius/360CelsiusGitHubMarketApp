420a2d71de94d251f62b1d7e6f445d6d85aff874
dennisshar <dennis.shar@gmail.com>
2017-10-31 15:33:07 +0200

    getTradeTimeStamp connected

--- StocksAlmanac/app/src/main/java/com/a360/celsius/stocksalmanac/listadapter/QoutesDataLIstCustomAdapter.java
+++ StocksAlmanac/app/src/main/java/com/a360/celsius/stocksalmanac/listadapter/QoutesDataLIstCustomAdapter.java
@@ -14,9 +14,17 @@ import com.a360.celsius.stocksalmanac.R;
 import com.a360.celsius.stocksalmanac.datamodel.QuoteItemDataModel;
 import com.a360.celsius.stocksalmanac.datamodel.SideMenuItemDataModel;
 
+import org.joda.time.DateTime;
+import org.joda.time.LocalDateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
 import org.w3c.dom.Text;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by dennisshar on 24/10/2017.
@@ -44,6 +52,7 @@ public class QoutesDataLIstCustomAdapter extends ArrayAdapter<QuoteItemDataModel
         TextView close;
         TextView low;
         TextView high;
+        TextView lastUpdate;
     }
 
     public QoutesDataLIstCustomAdapter(ArrayList<QuoteItemDataModel> data, Context context) {
@@ -77,7 +86,7 @@ public class QoutesDataLIstCustomAdapter extends ArrayAdapter<QuoteItemDataModel
             viewHolder.close = (TextView) convertView.findViewById(R.id.close_value);
             viewHolder.low = (TextView) convertView.findViewById(R.id.low_value);
             viewHolder.high = (TextView) convertView.findViewById(R.id.high_value);
-
+            viewHolder.lastUpdate = (TextView) convertView.findViewById(R.id.quote_last_updated_value);
             //result=convertView;
 
             convertView.setTag(viewHolder);
@@ -111,6 +120,20 @@ public class QoutesDataLIstCustomAdapter extends ArrayAdapter<QuoteItemDataModel
         viewHolder.low.setText(String.valueOf(dataModel.getLow()));
         viewHolder.high.setText(String.valueOf(dataModel.getHigh()));
 
+        String dateString = dataModel.getTradeTimeStamp();//"2010-03-01T00:00:00-08:00";
+        String pattern = "yyyy-MM-dd'T'HH:mm:ss";
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        Date date = null;
+        String dateToString = null;
+        try {
+            date = sdf.parse(dateString);
+            dateToString = date.toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+            dateToString = "N/A";
+        }
+
+        viewHolder.lastUpdate.setText(dateToString);
 
         // Return the completed view to render on screen
         return convertView;
