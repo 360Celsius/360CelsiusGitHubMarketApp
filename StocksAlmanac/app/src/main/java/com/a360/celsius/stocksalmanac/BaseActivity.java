be19ce01eca0e1001038e6f800e737eefec40a8f
dennisshar <dennis.shar@gmail.com>
2017-11-01 19:21:38 +0200

    some ui fixes

--- StocksAlmanac/app/src/main/java/com/a360/celsius/stocksalmanac/BaseActivity.java
+++ StocksAlmanac/app/src/main/java/com/a360/celsius/stocksalmanac/BaseActivity.java
@@ -1,8 +1,11 @@
 package com.a360.celsius.stocksalmanac;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.a360.celsius.stocksalmanac.dbhelper.DatabaseHelper;
 import com.a360.celsius.stocksalmanac.interfaces.DBhelperInterface;
@@ -19,6 +22,12 @@ public class BaseActivity extends AppCompatActivity implements DBhelperInterface
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.tool_bar_color));
+        }
 
         // In any activity just pass the context and use the singleton method
         helper = DatabaseHelper.getInstance(getApplicationContext());
