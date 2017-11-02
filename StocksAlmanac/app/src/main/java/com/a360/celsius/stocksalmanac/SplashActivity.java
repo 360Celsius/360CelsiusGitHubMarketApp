9418056a9b8743041b944338c0d90255f1141468
dennisshar <dennis.shar@gmail.com>
2017-11-02 22:51:26 +0200

    splash activity full screen

--- StocksAlmanac/app/src/main/java/com/a360/celsius/stocksalmanac/SplashActivity.java
+++ StocksAlmanac/app/src/main/java/com/a360/celsius/stocksalmanac/SplashActivity.java
@@ -2,6 +2,7 @@ package com.a360.celsius.stocksalmanac;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.a360.celsius.stocksalmanac.service.StockDataPullService;
 import com.a360.celsius.stocksalmanac.service.StockDataPullServiceIntentKeys;
@@ -15,6 +16,15 @@ public class SplashActivity extends BaseActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
         setContentView(R.layout.splash_activity);
 
         Intent msgIntent = new Intent(getApplicationContext(), StockDataPullService.class);
